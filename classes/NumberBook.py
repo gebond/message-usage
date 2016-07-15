a2053c93041838ec3613da4a27c9d2023c5497c8
gebond <gebond77@gmail.com>
2016-07-15 15:09:31 +0300

    fix wrong validation logic

--- classes/NumberBook.py
+++ classes/NumberBook.py
@@ -7,17 +7,20 @@ class NumberBook:
         self.__numbers = []
 
     def addNumber(self, numberItem):
-        for num in self.__numbers:
-            if num.getNumber() == numberItem.getNumber():
-                print("\t -> already exists " + numberItem.getNumber())
-                break
-            if num.getNumber()[0] is not "8":
-                print("\t -> suspecious number! " + numberItem.getNumber())
-
+        if self.__validateNumber(numberItem):
             self.__numbers.append(numberItem)
             print("\t -> added " + numberItem.getNumber())
 
 
+    def __validateNumber(self, number):
+        for num in self.__numbers:
+            if num.getNumber() == number.getNumber():
+                print("\t -> already exists " + number.getNumber())
+                return False
+        if number.getNumber()[0] is not "8":
+            print("\t -> suspecious number! " + number.getNumber())
+            return False
+        return True
 
     def showNumbers(self):
         for num in self.__numbers:
