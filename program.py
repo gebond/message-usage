8c15eaca98f24d1606e4ce7620d24f4b536c0ecf
gebond <gebond77@gmail.com>
2016-07-18 12:44:26 +0300

    fix relating path to file

--- program.py
+++ program.py
@@ -4,8 +4,7 @@ from classes.NumberManager import NumberManager
 print("Hello")
 
 manager = NumberManager()
-#path = "C:\\Users\\glbo0616\\repository\\myrep\\message-usage\\message-usage\\sourse-numbers\\"
-path = "C:\\Users\\Gleb\\PycharmProjects\\message-usage\\sourse-numbers\\"
+path = ".\\sourse-numbers\\"
 file = "my"
 
 manager.readNumbers(file, path, " ")
